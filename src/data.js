3f7a3663611fba1fbd87a3d96dc6b8647304715a
claudiakemi <claudiakemis@gmail.com>
2019-09-13 00:58:38 -0300

    Lista de pokemons com num, name e img + css

--- src/data.js
+++ src/data.js
@@ -1,17 +1,7 @@
 // esta é uma função de exemplo
 // veja como agregamos a função ao objeto global window
-let arrayPokemons = POKEMON.pokemon;
-function listComplete () {
-for (let i = 0; i < arrayPokemons.length; i++) {
-  console.log(arrayPokemons[i].name);
-  // listOfPokemons.push(i);
-  // return arrayPokemons;
-// }
-// }
-// POKEMON.forEach(changeTitleToPokemon);
-// function changeTitleToPokemon(name){
-}
-}
+
+
 
 // const example = () => {
 //
--- src/main.js
+++ src/main.js
@@ -4,24 +4,17 @@ document.getElementById("eggs-btn").addEventListener("click", changeTitleToEggs)
 function changeTitleToPokemon() {
   document.getElementById("showComboBox").innerHTML= '<select id="filters"><option value="0">Pokedéx</option><option value="1">Ordem Alfabética</option></select>';
   document.getElementById("pageTitle").innerHTML = "POKÉMONS";
-  //document.head.appendChild(pokemonInfo);
-
-    //return listOfPokemons;
-    // let pokemonInfo = document.createElement("div");
-    // let pokemonName = POKEMON.pokemon[i].name;
-    // let pokemonImg = POKEMON.pokemon[i].img;
-  //const appearList = listComplete(POKEMON);
-  //document.getElementById("buttons").innerHTML= POKEMON.pokemon;
-    // document.getElementById("buttons").innerHTML= pokemonName;
-    // document.getElementById("buttons").innerHTML= pokemonImg;
+  document.getElementById("buttons").innerHTML ="";
+  POKEMON.pokemon.forEach(myFunction);
+  function myFunction(item) {
+  document.getElementById("ListById").innerHTML += "Pokedéx:" + item.num + "/" + item.name +'<img src="'+item.img+'" >'+"<br>";
   }
 
-  //`<p>Lista de pokémons: ${função que filtra os pokemons pelo num e img}</p>`;
+//document.head.appendChild(pokemonInfo);
+// let pokemonInfo = document.createElement("div");
+//  document.getElementById("buttons").innerHTML= `<p>Lista de pokémons: ${}</p>`;
 
-  //document.getElementById("filters").addEventListener("valueChange", filteredBy);
-
-
-
+  }
 function changeTitleToEggs() {
   document.getElementById("pageTitle").innerHTML = "OVOS";
   document.getElementById("buttons").innerHTML= "Lista de ovos";
