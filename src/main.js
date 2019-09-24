446fd2ecfa6f2a61c4a136ac562f448017223a26
fernanda <ono.frnd@gmail.com>
2019-09-24 16:35:50 -0300

    inclusão da ordenação por n da pokedex

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ document.getElementById("eggs-btn").addEventListener("click", changeTitleToEggs)
 
 //função que muda os elementos do HTML após clicar no botão "Pokémons"
 function changeTitleToPokemon() {
-  document.getElementById("showComboBox").innerHTML = '<select id="order"><option value="num">Pokedéx</option><option value="AZ">A-Z</option><option value="ZA">Z-A</option></select>';
+  document.getElementById("showComboBox").innerHTML = '<select id="order"><option value="num">Pokedéx 1 a 151</option><option value="num2">Pokedéx 151 a 1</option><option value="AZ">A-Z</option><option value="ZA">Z-A</option></select>';
   document.getElementById("order").addEventListener("click", changeOrder);
   document.getElementById("pageTitle").innerHTML = "POKÉMONS";
   document.getElementById("buttons").innerHTML = "";
@@ -19,21 +19,26 @@ function printPokemons(arr) {
 
 function changeOrder() {
   let order = document.getElementById("order");
-  if (order.value === "AZ") {
-    data.sort(function (a, b) {
-      return a.name.localeCompare(b.name, "en", { sensitivity: "base" });
-    });
-  } else if (order.value === "ZA") {
-    data.sort(function (a, b) {
-      return b.name.localeCompare(a.name, "en", { sensitivity: "base" })
-    });
-  } else {
+  if(order.value === "num") {
     data.sort(function (a, b) {
       return a.num.localeCompare(b.num);
+  });
+  } else if(order.value === "num2") {
+      data.sort(function (a, b) {
+        return b.num.localeCompare(a.num);
     });
-  }
-  printPokemons(data);
+      } else if(order.value === "AZ") {
+        data.sort(function (a, b) {
+          return a.name.localeCompare(b.name, "en", { sensitivity: "base" });
+      });
+        } else {
+        data.sort(function (a, b) {
+          return b.name.localeCompare(a.name, "en", { sensitivity: "base" });
+    });
+        }
+            printPokemons(data);
 }
+
 //função que muda os elementos do HTML após clicar no botão "Ovos" e chama a função de filtro dos ovos
 function changeTitleToEggs() {
   //document.getElementById("showComboBox").innerHTML = '<select id="eggFilters"><option value="none">Selecione uma kilometragem</option><option value="2 km">2 km</option><option value="5 km">5 km</option><option value="10 km">10 km</option></select>';
