c78fe7e392c5dbdc802ca9c3f78ddd8459668738
fernanda <ono.frnd@gmail.com>
2019-09-28 12:18:21 -0300

    texto do calculo

--- src/main.js
+++ src/main.js
@@ -40,22 +40,22 @@ function changeTitleToEggs() {
     const valor = e.target.value;
     const egg = app.filterEggsByKm(data, valor);
     newCards(egg);
-    let porcentagem = Math.round(parseFloat((egg.length)/151*100));
-    document.getElementById("result").innerHTML = porcentagem + "% de 151 pokémons aparecem em ovos de 2km";
+    let porcentagem2km = Math.round(parseFloat((egg.length)/151*100));
+    document.getElementById("result").innerHTML = porcentagem2km + "% de 151 pokémons aparecem em ovos de 2km";
   });
   eggFilter5km.addEventListener("click", (e) => {
     const valor = e.target.value;
     const egg = app.filterEggsByKm(data, valor);
     newCards(egg);
-    let porcentagem = Math.round(parseFloat((egg.length)/151*100));
-    document.getElementById("result").innerHTML = porcentagem + "% de 151 pokémons aparecem em ovos de 5km";
+    let porcentagem5km = Math.round(parseFloat((egg.length)/151*100));
+    document.getElementById("result").innerHTML = porcentagem5km + "% de 151 pokémons aparecem em ovos de 5km";
   });
   eggFilter10km.addEventListener("click", (e) => {
     const valor = e.target.value;
     const egg = app.filterEggsByKm(data, valor);
     newCards(egg);
-    let porcentagem = Math.round(parseFloat((egg.length)/151*100));
-    document.getElementById("result").innerHTML = porcentagem + "% de 151 pokémons aparecem em ovos de 10km";
+    let porcentagem10km = Math.round(parseFloat((egg.length)/151*100));
+    document.getElementById("result").innerHTML = porcentagem10km + "% de 151 pokémons aparecem em ovos de 10km";
   });
 
   //função que cria os cards de pokemons depois que a kilometragem de ovos foi selecionada no dropdown
